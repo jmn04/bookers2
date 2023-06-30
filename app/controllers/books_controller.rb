41c380ff8dc7adb145d15bf76a7a31c017bc6f16
EC2 Default User <ec2-user@ip-172-31-36-89.ap-northeast-1.compute.internal>
2023-06-30 07:29:03 +0000

    [Finish] 4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,66 @@
 class BooksController < ApplicationController
-  def index
-  end
+  before_action :is_matching_login_user, only: [:edit, :update]
   
-  def new
-    @post_image = User.new
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
-    @post_image = PostImage.new(post_image_params)
-    @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
+  def show
+   @book = Book.find(params[:id])
+   @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @books = Book.where(id: params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully deleted."
+    redirect_to "/books"
   end
   
   #ここからプライベート
   private
 
-  def post_image_params
-    params.require(:post_image).permit(:shop_name, :image, :caption)
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+  
+  # 他ユーザーからのアクセス制限
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/show'
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :books
   # resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 end
